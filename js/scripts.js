f4481282a2798e1cb88104383b0117aaf7067042
Muhammad Arham Jamil <codewitharham.remote@gmail.com>
2025-04-16 23:26:05 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Example: Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('show');
+}
+
+
+// 
